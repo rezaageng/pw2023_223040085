7c798f6519a0b13ec93e749afbe21e211d3a19e6
rezaa <rezaageng5@gmail.com>
2023-03-23 16:08:45 +0700

    feat(tubes): :sparkles: add news page

--- tubes/src/news.php
+++ tubes/src/news.php
@@ -47,20 +47,7 @@
   </nav>
 
   <main class="max-w-7xl m-auto p-4">
-    <div class="group relative aspect-[3/4] rounded-3xl overflow-clip sm:aspect-video">
-      <a href="#">
-        <img src="./images/banner.jpeg" alt="banner" class="h-full w-full
-        object-cover group-hover:scale-110 transition duration-200">
-        <div class="absolute z-10 bottom-0 left-0 bg-gradient-to-t from-gray-800 w-full h-full"></div>
-        <div class="absolute z-10 bottom-0 left-0 p-4 sm:p-8 max-w-4xl">
-          <h1 class="text-2xl font-bold truncate sm:text-4xl">Genshin Impact 3.5 has released</h1>
-          <p class="line-clamp-2 sm:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsa voluptatem aperiam perspiciatis maiores, placeat atque nostrum culpa accusamus possimus animi sunt nam ducimus repellat quaerat, dolorum totam reprehenderit labore?</p>
-        </div>
-      </a>
-    </div>
-
-    <div class="mt-8 flex flex-col mb-12 gap-4">
-      <h2 class="font-bold text-xl">Latest News</h2>
+    <div class="flex flex-col mb-12 gap-4">
       <div class="grid w-full grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 md:gap-3 lg:grid-cols-4">
         <div class="group">
           <a class="flex flex-col gap-2" href="#">
