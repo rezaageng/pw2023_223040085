c7da6efe083b70309b605af3619ce8999a1ab964
rezaa <rezaageng5@gmail.com>
2023-03-26 10:55:24 +0700

    feat(tubes): :sparkles: add form

--- tubes/src/admin/form.php
+++ tubes/src/admin/form.php
@@ -39,20 +39,52 @@
   </nav>
 
   <main class="max-w-7xl m-auto p-4">
-    <div class="mb-4 w-full flex justify-between items-center">
-      <h1 class="text-xl font-bold">News</h1>
-      <button onclick="document.location.href = './form.php'" class="bg-teal-600 rounded-xl px-4 py-2">Add</button>
-    </div>
-    <div class="flex flex-col w-full bg-gray-800 rounded-xl gap-4 overflow-clip">
-      <div class="p-4">
-        <h1 class="text-xl font-bold line-clamp-1">Wanderer</h1>
-        <p class="line-clamp-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium ipsum error ipsam laborum voluptate similique odit est, soluta officia et distinctio laudantium enim ad dolor? Impedit delectus autem neque id, sunt magnam laborum minima eligendi quos odit dolorum pariatur iste labore beatae porro. Deserunt incidunt cum, illo eos adipisci ducimus rerum suscipit quae esse. Unde ipsam voluptates quia, quae perferendis illum in vitae asperiores dolorem suscipit modi corrupti adipisci eveniet dolores quidem debitis tenetur commodi nam odit animi sapiente quam optio est. Aperiam, harum labore libero nulla dolores quia incidunt quo, quae corporis soluta vel eligendi qui eius. Quisquam, eum?</p>
+    <form action="">
+      <div class="flex justify-end">
+        <button class="bg-indigo-600 px-4 py-2 rounded-xl mb-4">Publish</button>
       </div>
-      <div class="flex justify-around">
-        <button class="bg-indigo-600 flex-1 p-2">Edit</button>
-        <button class="bg-rose-600 flex-1 p-2">Delete</button>
-      </div>
-    </div>
+      <div class="bg-gray-800 p-4 rounded-xl">
+        <div class="flex flex-col gap-4">
+          <div class="flex flex-col gap-2">
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" class="border bg-gray-700 caret-teal-600 border-gray-600 rounded-lg p-2">
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="content">Content</label>
+            <textarea name="content" id="content" cols="30" rows="10" class="border bg-gray-700 caret-teal-600 border-gray-600 rounded-lg p-2"></textarea>
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="image">Image</label>
+            <input type="file" name="image" id="image" class="border bg-gray-700 caret-teal-600 border-gray-600 rounded-lg p-2">
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="category">Category</label>
+            <select name="category" id="category" class="border bg-gray-700 caret-teal-600 border-gray-600 rounded-lg p-2">
+              <option value="1">Category 1</option>
+              <option value="2">Category 2</option>
+              <option value="3">Category 3</option>
+            </select>
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="author">Author</label>
+            <select name="author" id="author" class="border bg-gray-700 caret-teal-600 border-gray-600 rounded-lg p-2">
+              <option value="1">Author 1</option>
+              <option value="2">Author 2</option>
+              <option value="3">Author 3</option>
+            </select>
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="date">Date</label>
+            <input type="date" name="date" id="date" class="border bg-gray-700 caret-teal-600 border-gray-600 rounded-lg p-2">
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="status">Status</label>
+            <select name="status" id="status" class="border bg-gray-700 caret-teal-600 border-gray-600 rounded-lg p-2">
+              <option value="1">Published</option>
+              <option value="2">Draft</option>
+            </select>
+          </div>
+    </form>
   </main>
 
 </body>
